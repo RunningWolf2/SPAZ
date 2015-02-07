fd301a9dc45f8f12fd9c441ab6ed9513729f7b95
Andre Meyering <info@andremeyering.de>
2015-02-07 21:19:32 +0100

    Tabellen Migration erstellen
    - Die Tabellen Migrationsdateien für 3 neue Tabellen erstellt
    - Users/Mitarbeiter: zwei Spalten hinzugefügt

---
+++ SPAZ/database/migrations/2015_02_07_194453_create_familien_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Familien extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('familien', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('anrede');
+			$table->string('name');
+			$table->string('strasse');
+			$table->string('plz');
+			$table->string('ort');
+			$table->string('telefon');
+			$table->string('mobil');
+			$table->string('fax');
+			$table->string('email');
+			$table->decimal('bewilligteFahrzeit', 6, 2);
+			$table->integer('refJugendamt');
+			$table->integer('refMitarbeiter');
+			$table->timestamp('startBetreuung');
+			$table->timestamp('endBetreuung');
+			$table->string('status');
+			$table->string('refAnsprechpartner');
+			$table->string('refWeitereAdressen');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('familien');
+	}
+
+}
---
+++ SPAZ/database/migrations/2015_02_07_194512_create_familien_ansprechpartner_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class FamilienAnsprechpartner extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('familien_ansprechpartner', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('typ');
+			$table->string('anrede');
+			$table->string('vorname');
+			$table->string('nachname');
+			$table->string('strasse');
+			$table->string('plz');
+			$table->string('ort');
+			$table->string('telefon');
+			$table->string('mobil');
+			$table->string('fax');
+			$table->string('email');
+			$table->longText('sonstiges');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('familien_ansprechpartner');
+	}
+
+}
