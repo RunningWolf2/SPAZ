9b321c665a7aa6c255793951119d071dd9fc2a34
Andre Meyering <info@andremeyering.de>
2015-02-21 12:15:59 +0100

    404 & 403 Error Meldung hinzugefügt

---
+++ SPAZ/resources/views/errors/403.blade.php
@@ -0,0 +1,21 @@
+@extends('app')
+
+@section('content')
+
+<div class="container">
+	<div class="row">
+		<div class="small-10 small-offset-1">
+
+			<h2>Nicht berechtigt!</h2>
+
+			<div class="radius panel">
+
+				<p>Sie sind nicht berechtigt, diese Seite zu betrachten.</p>
+
+			</div>
+
+		</div>
+	</div>
+</div>
+
+@endsection
---
+++ SPAZ/resources/views/errors/404.blade.php
@@ -0,0 +1,21 @@
+@extends('app')
+
+@section('content')
+
+<div class="container">
+	<div class="row">
+		<div class="small-10 small-offset-1">
+
+			<h2>Nicht gefunden!</h2>
+
+			<div class="radius panel">
+
+				<p>Die angeforderte Seite wurde nicht gefunden!</p>
+
+			</div>
+
+		</div>
+	</div>
+</div>
+
+@endsection
